62ce75faad5f70c03eeae65df5d70438409902e9
Ahmet Can Buyukyilmaz <abuyukyi@purdue.edu>
2023-04-14 00:01:31 +0300

    removed unused scripts

--- scripts/add.js
+++
@@ -1,9 +0,0 @@
-const adder = document.querySelector('#adder');
-const addButton = adder.children[0];
-const addSections = adder.children[1];
-
-addButton.addEventListener('click', () => {
-    const isExpanded = addSections.style.getPropertyValue('visibility') === 'visible';
-    addButton.children[0].setAttribute('transform', `rotate(${isExpanded ? '0' : '45'}, 12, 12)`);
-    addSections.style.setProperty('visibility', isExpanded ? 'hidden' : 'visible');
-});
