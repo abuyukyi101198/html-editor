e1c9a2fd0c44ac78dcf082b00134ae018de403f6
Ahmet Can Buyukyilmaz <abuyukyi@purdue.edu>
2023-04-15 00:02:43 +0300

    added editor filling functions

--- scripts/sectionHandler.js
+++ scripts/sectionHandler.js
@@ -27,10 +27,7 @@ function setSelect(section) {
     section.addEventListener('click', (e) => {
         if (!section.children[0].children[0].contains(e.target)
             && !section.children[0].children[1].contains(e.target)) {
-            if (section.classList.contains('active')) {
-                handleUnselect(section);
-            }
-            else {
+            if (!section.classList.contains('active')) {
                 handleSelect(section);
             }
         }
@@ -44,11 +41,18 @@ function setSelect(section) {
 function handleSectionAdd(preview, button) {
     const section = document.createElement('li');
     section.setAttribute('class', 'section');
+    section.setAttribute('data-type', button.getAttribute('id'));
     section.innerHTML += moveHTML;
 
     const content = document.createElement('div');
     content.setAttribute('class', 'content');
-    content.innerHTML += sectionHTMLs[button.getAttribute('id')];
+    if (section.getAttribute('data-type') === 'textimage') {
+        section.setAttribute('data-alignment', 'left-alignment');
+        content.innerHTML += sectionHTMLs[button.getAttribute('id')]['left'];
+    }
+    else {
+        content.innerHTML += sectionHTMLs[button.getAttribute('id')];
+    }
     section.appendChild(content);
     section.innerHTML += deleteHTML;
     preview.appendChild(section);
@@ -56,6 +60,8 @@ function handleSectionAdd(preview, button) {
     setMove(section);
 
     setSelect(section);
+
+    handleSelect(section);
 }
 
 function handleSelect(section) {
@@ -67,6 +73,7 @@ function handleSelect(section) {
     controls.forEach(control => {
         control.style.setProperty('visibility', 'visible');
     });
+    fillEditorContainer(section);
     if (!section.classList.contains('active')) {
         section.classList.add('active');
     }
@@ -79,3 +86,46 @@ function handleUnselect(section) {
     });
     section.classList.remove('active');
 }
+
+function fillEditorContainer(section) {
+    const sectionType = section.getAttribute('data-type');
+
+    const alignment = document.querySelector('#alignment');
+    const imageDrop = document.querySelector('#image-drop');
+    const textEditor = document.querySelector('.standalone-container');
+
+    if (sectionType === 'header' || sectionType === 'footer') {
+        alignment.classList.add('unavailable');
+        imageDrop.classList.add('unavailable');
+        textEditor.classList.add('unavailable');
+    }
+    else {
+        if (sectionType === 'text') {
+            alignment.classList.add('unavailable');
+            imageDrop.classList.add('unavailable');
+            textEditor.classList.remove('unavailable');
+            fillTextEditor(section);
+        }
+        else if (sectionType === 'textimage') {
+            alignment.classList.remove('unavailable');
+            imageDrop.classList.remove('unavailable');
+            textEditor.classList.remove('unavailable');
+            fillTextEditor(section);
+        }
+        else if (sectionType === 'image') {
+            alignment.classList.add('unavailable');
+            imageDrop.classList.remove('unavailable');
+            textEditor.classList.add('unavailable');
+        }
+    }
+}
+
+function fillTextEditor(section) {
+    const textTd = section.querySelector('.text-td');
+
+    const value = textTd.innerHTML;
+    const delta = quill.clipboard.convert(value);
+
+    quill.setContents(delta, 'silent');
+}
+
