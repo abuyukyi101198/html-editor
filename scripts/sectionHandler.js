de6f7352fc7221955ca871a598ee0fd32705ddbb
Ahmet Can Buyukyilmaz <abuyukyi@purdue.edu>
2023-04-16 00:08:37 +0300

    cleaned sectionHandler.js

--- scripts/sectionHandler.js
+++ scripts/sectionHandler.js
@@ -1,16 +1,39 @@
 (function () {
-    const addSections = document.querySelector('#add-sections');
-
-    const preview = document.querySelector('#preview');
-
-    const sections = [...addSections.children];
-
-    sections.forEach(button => {
-        button.addEventListener('click', () => {
-            handleSectionAdd(preview, button);
-        });
-    });
+    const sections = document.querySelectorAll('#add-sections .button');
+    sections.forEach(btn => {
+        btn.addEventListener('click', () => handleSectionAdd(btn));
+    })
 })();
+
+function handleSectionAdd(button) {
+    const preview = document.querySelector('#preview');
+    const section = document.createElement('li');
+
+    section.setAttribute('class', 'section');
+    section.setAttribute('data-type', button.getAttribute('id'));
+
+    section.innerHTML += moveHTML;
+
+    const content = document.createElement('div');
+    content.setAttribute('class', 'content');
+
+    if (section.getAttribute('data-type') === 'textimage') {
+        section.setAttribute('data-alignment', 'left-alignment');
+        content.innerHTML += sectionHTMLs[button.getAttribute('id')]['left'];
+    }
+    else {
+        content.innerHTML += sectionHTMLs[button.getAttribute('id')];
+    }
+
+    section.appendChild(content);
+    section.innerHTML += deleteHTML;
+
+    preview.appendChild(section);
+
+    setMove(section);
+    setSelect(section);
+    handleSelect(section);
+}
 
 function setMove(section) {
     section.children[0].addEventListener('click', (e) => {
@@ -35,33 +58,16 @@ function setSelect(section) {
 
     section.querySelector('.delete').addEventListener('click', () => {
         section.remove();
+        const alignment = document.querySelector('#alignment');
+        const imageDrop = document.querySelector('#image-drop');
+        const textEditor = document.querySelector('.standalone-container');
+        const imageInput = imageDrop.querySelector('input');
+
+        alignment.classList.add('unavailable');
+        imageDrop.classList.add('unavailable');
+        textEditor.classList.add('unavailable');
+        imageInput.setAttribute('disabled', '');
     });
-}
-
-function handleSectionAdd(preview, button) {
-    const section = document.createElement('li');
-    section.setAttribute('class', 'section');
-    section.setAttribute('data-type', button.getAttribute('id'));
-    section.innerHTML += moveHTML;
-
-    const content = document.createElement('div');
-    content.setAttribute('class', 'content');
-    if (section.getAttribute('data-type') === 'textimage') {
-        section.setAttribute('data-alignment', 'left-alignment');
-        content.innerHTML += sectionHTMLs[button.getAttribute('id')]['left'];
-    }
-    else {
-        content.innerHTML += sectionHTMLs[button.getAttribute('id')];
-    }
-    section.appendChild(content);
-    section.innerHTML += deleteHTML;
-    preview.appendChild(section);
-
-    setMove(section);
-
-    setSelect(section);
-
-    handleSelect(section);
 }
 
 function handleSelect(section) {
@@ -69,11 +75,14 @@ function handleSelect(section) {
     if (active !== null) {
         handleUnselect(active);
     }
+
     const controls = section.querySelectorAll('.control');
     controls.forEach(control => {
         control.style.setProperty('visibility', 'visible');
     });
+
     fillEditorContainer(section);
+
     if (!section.classList.contains('active')) {
         section.classList.add('active');
     }
@@ -81,9 +90,11 @@ function handleSelect(section) {
 
 function handleUnselect(section) {
     const controls = section.querySelectorAll('.control');
+
     controls.forEach(control => {
         control.style.setProperty('visibility', 'hidden');
     });
+
     section.classList.remove('active');
 }
 
@@ -94,29 +105,45 @@ function fillEditorContainer(section) {
     const imageDrop = document.querySelector('#image-drop');
     const textEditor = document.querySelector('.standalone-container');
 
+    const imageInput = imageDrop.querySelector('input');
+
     if (sectionType === 'header' || sectionType === 'footer') {
-        alignment.classList.add('unavailable');
-        imageDrop.classList.add('unavailable');
-        textEditor.classList.add('unavailable');
+        setFields([true, true, true, true]);
     }
-    else {
-        if (sectionType === 'text') {
+    else if (sectionType === 'text') {
+        setFields([true, true, false, true]);
+        fillTextEditor(section);
+    }
+    else if (sectionType === 'textimage') {
+        setFields([false, false, false, false]);
+        fillTextEditor(section);
+        fillImageDrop(imageDrop, section);
+    }
+    else if (sectionType === 'image') {
+        setFields([true, false, true, false]);
+        fillImageDrop(imageDrop, section);
+    }
+
+    function setFields(fieldParams) {
+        if (fieldParams[0])
             alignment.classList.add('unavailable');
-            imageDrop.classList.add('unavailable');
-            textEditor.classList.remove('unavailable');
-            fillTextEditor(section);
-        }
-        else if (sectionType === 'textimage') {
+        else
             alignment.classList.remove('unavailable');
+
+        if (fieldParams[1])
+            imageDrop.classList.add('unavailable');
+        else
             imageDrop.classList.remove('unavailable');
-            textEditor.classList.remove('unavailable');
-            fillTextEditor(section);
-        }
-        else if (sectionType === 'image') {
-            alignment.classList.add('unavailable');
-            imageDrop.classList.remove('unavailable');
+
+        if (fieldParams[2])
             textEditor.classList.add('unavailable');
-        }
+        else
+            textEditor.classList.remove('unavailable');
+
+        if (fieldParams[3])
+            imageInput.setAttribute('disabled', '');
+        else
+            imageInput.removeAttribute('disabled');
     }
 }
 
@@ -129,3 +156,20 @@ function fillTextEditor(section) {
     quill.setContents(delta, 'silent');
 }
 
+function fillImageDrop(imageDrop, section) {
+    let svg = section.querySelector('.image-td svg');
+
+    if (!svg) return;
+
+    svg = svg.cloneNode(true);
+    svg.style.setProperty('margin-top', '0');
+    svg.style.setProperty('height', '80%');
+    svg.style.setProperty('width', '85%');
+
+    if (imageDrop.getAttribute('data-preview')) {
+        imageDrop.lastChild.remove();
+    }
+
+    imageDrop.appendChild(svg);
+    imageDrop.setAttribute('data-preview', true);
+}
